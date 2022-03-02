51bbbb7b6d744bd365e7dfe51e840d0267c2af3f
CamBrickl <79422034+CamBrickl@users.noreply.github.com>
2022-03-02 16:52:58 -0600

    Trying to fix

--- BloomGame/Character.gd
+++ BloomGame/Character.gd
@@ -1,23 +1,21 @@
 extends KinematicBody2D
 
-var velocity = Vector2(0,0)
-const SPEED = 50
-const GRAVITY = 30
-const JUMP = -900
+export (int) var speed = 200
+
+var velocity = Vector2()
+
+func get_input():
+	velocity = Vector2()
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
+	velocity = velocity.normalized() * speed
 
 func _physics_process(delta):
-#	velocity = Vector2()
-	if Input.is_action_pressed("Right"):
-		velocity.x += SPEED
-	if Input.is_action_pressed("Left"):
-		velocity.x -= SPEED
-#	if Input.is_action_pressed("ui_down"):
-#		velocity.y += 1
-#	if Input.is_action_pressed("ui_up"):
-#		velocity.y -= 1
-	velocity.y = velocity.y + GRAVITY
-	if Input.is_action_just_pressed("Jump") and is_on_floor():
-		velocity.y = JUMP
-#	velocity = velocity.normalized() * speed
-
+	get_input()
 	velocity = move_and_slide(velocity)
