acddabb0690f9084590b482071c4bca120ef0e62
CamBrickl <79422034+CamBrickl@users.noreply.github.com>
2022-03-02 16:46:40 -0600

    Updated gravity and movement

--- BloomGame/Character.gd
+++ BloomGame/Character.gd
@@ -1,21 +1,23 @@
 extends KinematicBody2D
 
-export (int) var speed = 200
-
-var velocity = Vector2()
-
-func get_input():
-	velocity = Vector2()
-	if Input.is_action_pressed("ui_right"):
-		velocity.x += 1
-	if Input.is_action_pressed("ui_left"):
-		velocity.x -= 1
-	if Input.is_action_pressed("ui_down"):
-		velocity.y += 1
-	if Input.is_action_pressed("ui_up"):
-		velocity.y -= 1
-	velocity = velocity.normalized() * speed
+var velocity = Vector2(0,0)
+const SPEED = 50
+const GRAVITY = 30
+const JUMP = -900
 
 func _physics_process(delta):
-	get_input()
+#	velocity = Vector2()
+	if Input.is_action_pressed("Right"):
+		velocity.x += SPEED
+	if Input.is_action_pressed("Left"):
+		velocity.x -= SPEED
+#	if Input.is_action_pressed("ui_down"):
+#		velocity.y += 1
+#	if Input.is_action_pressed("ui_up"):
+#		velocity.y -= 1
+	velocity.y = velocity.y + GRAVITY
+	if Input.is_action_just_pressed("Jump") and is_on_floor():
+		velocity.y = JUMP
+#	velocity = velocity.normalized() * speed
+
 	velocity = move_and_slide(velocity)
