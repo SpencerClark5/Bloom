868f81ca8d4404cd19d5fe08328b723ef2135c28
Rdawg028 <verdouwr1827@my.uwstout.edu>
2022-03-03 11:07:15 -0600

    trying to get walk cycle

--- BloomGame/Character.gd
+++ BloomGame/Character.gd
@@ -5,10 +5,14 @@ var velocity = Vector2(0,0)
 const SPEED = 50
 const GRAVITY = 30
 const JUMP = -900
+onready var _animated_sprite = $AnimatedSprite
+onready var _kinematicBody2D = $KinematicBody
+
 func _physics_process(delta):
 	
 	if Input.is_action_pressed("Right"):
 		velocity.x += SPEED
+		
 	if Input.is_action_pressed("Left"):
 		velocity.x -= SPEED
 	
