1e3de310abfaf58c2fe04dc3d1bfc085ab785dc1
CamBrickl <79422034+CamBrickl@users.noreply.github.com>
2022-03-04 16:32:17 -0600

    Removed old art and added comments

--- BloomGame/Character.gd
+++ BloomGame/Character.gd
@@ -9,6 +9,7 @@ onready var _animated_sprite = $AnimatedSprite
 
 func _physics_process(delta):
 	
+	# Movement and animation 
 	if Input.is_action_pressed("Right"):
 		_animated_sprite.play("Walking")
 		_animated_sprite.flip_h = false
@@ -22,12 +23,15 @@ func _physics_process(delta):
 		_animated_sprite.stop()
 		
 	
+	# Gravity
 	velocity.y = velocity.y + GRAVITY
 	
+	# Jump
 	if Input.is_action_just_pressed("Jump"):
 		velocity.y = JUMP
 
 
 	velocity = move_and_slide(velocity)
 	
+	# Friction
 	velocity.x = lerp(velocity.x,0,0.1)
