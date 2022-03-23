b8ffe2b6843104112013fea004718dc9abd6345b
CamBrickl <79422034+CamBrickl@users.noreply.github.com>
2022-03-23 16:45:37 -0500

    Good Luck Charlie

--- BloomGame/Scenes/Movement.gd
+++ BloomGame/Scenes/Movement.gd
@@ -11,11 +11,23 @@ onready var _animated_sprite_LeftArm = $LeftArm
 onready var _animated_sprite_LeftLeg = $LeftLeg
 onready var _animated_sprite_RightLeg = $RightLeg
 onready var _animated_sprite_Jump = $NewJumpingSprite
+onready var _animated_sprite_Idle = $IdleSprite
+onready var _animated_sprite_Plant = $PlantingSprite
 
 func _physics_process(delta):
 	
 	# Movement and animation 
 	if Input.is_action_pressed("Right"):
+		
+		_animated_sprite_Idle.visible = false
+		_animated_sprite_Body.visible = true
+		_animated_sprite_LeftArm.visible = true
+		_animated_sprite_LeftLeg.visible = true
+		_animated_sprite_RightArm.visible = true
+		_animated_sprite_RightLeg.visible = true
+		_animated_sprite_Jump.visible = false
+		
+		_animated_sprite_Idle.stop()
 		
 		_animated_sprite_Body.play("TorsoRunAnim")
 		_animated_sprite_Body.flip_h = false
@@ -35,6 +47,16 @@ func _physics_process(delta):
 		velocity.x += SPEED
 	
 	elif Input.is_action_pressed("Left"):
+		
+		_animated_sprite_Idle.visible = false
+		_animated_sprite_Body.visible = true
+		_animated_sprite_LeftArm.visible = true
+		_animated_sprite_LeftLeg.visible = true
+		_animated_sprite_RightArm.visible = true
+		_animated_sprite_RightLeg.visible = true
+		_animated_sprite_Jump.visible = false
+		_animated_sprite_Idle.stop()
+		
 		_animated_sprite_Body.play("TorsoRunAnim")
 		_animated_sprite_Body.flip_h = true
 		
@@ -52,30 +74,39 @@ func _physics_process(delta):
 		velocity.x -= SPEED
 	# Jump
 	elif Input.is_action_just_pressed("Jump"):
+		_animated_sprite_Idle.visible = false
 		_animated_sprite_Body.visible = false
 		_animated_sprite_LeftArm.visible = false
 		_animated_sprite_LeftLeg.visible = false
 		_animated_sprite_RightArm.visible = false
 		_animated_sprite_RightLeg.visible = false
+		
 		_animated_sprite_Jump.visible = true
 		_animated_sprite_Jump.play("NewJump")
-		velocity.y = JUMP
+#		velocity.y = JUMP
 	else:
-		_animated_sprite_Body.visible = true
-		_animated_sprite_LeftArm.visible = true
-		_animated_sprite_LeftLeg.visible = true
-		_animated_sprite_RightArm.visible = true
-		_animated_sprite_RightLeg.visible = true
+		_animated_sprite_Idle.visible = true
+		_animated_sprite_Idle.play("Idle")
+		
+		_animated_sprite_Body.visible = false
+		_animated_sprite_LeftArm.visible = false
+		_animated_sprite_LeftLeg.visible = false
+		_animated_sprite_RightArm.visible = false
+		_animated_sprite_RightLeg.visible = false
 		_animated_sprite_Jump.visible = false
-		_animated_sprite_Body.stop()
-		_animated_sprite_LeftArm.stop()
-		_animated_sprite_LeftLeg.stop()
-		_animated_sprite_RightArm.stop()
-		_animated_sprite_RightLeg.stop()
+		
+		
+#		_animated_sprite_Body.stop()
+#		_animated_sprite_LeftArm.stop()
+#		_animated_sprite_LeftLeg.stop()
+#		_animated_sprite_RightArm.stop()
+#		_animated_sprite_RightLeg.stop()
 	
 	# Gravity
 	velocity.y = velocity.y + GRAVITY
 	
+	if Input.is_action_just_pressed("Jump"):
+		velocity.y = JUMP
 	
 	velocity = move_and_slide(velocity)
 	
