59f69f7b76096371529482b34fc501cca12deb0d
CamBrickl <79422034+CamBrickl@users.noreply.github.com>
2022-03-28 17:35:30 -0500

    New Jump

--- BloomGame/Scenes/Movement.gd
+++ BloomGame/Scenes/Movement.gd
@@ -16,7 +16,7 @@ onready var _animated_sprite_Plant = $PlantingSprite
 
 var time_elapsed = 0.0
 
-var isOnGround = true
+var isOnGround = 0
 	
 func _physics_process(delta):
 	
@@ -49,6 +49,8 @@ func _physics_process(delta):
 			
 			_animated_sprite_RightLeg.play("RightLegRun")
 			_animated_sprite_RightLeg.flip_h = false
+			
+			isOnGround = 0
 		else:
 			_animated_sprite_Idle.visible = false
 			_animated_sprite_Body.visible = false
@@ -90,6 +92,8 @@ func _physics_process(delta):
 			
 			_animated_sprite_RightLeg.play("RightLegRun")
 			_animated_sprite_RightLeg.flip_h = true
+			
+			isOnGround = 0
 		else:
 			_animated_sprite_Idle.visible = false
 			_animated_sprite_Body.visible = false
@@ -116,8 +120,40 @@ func _physics_process(delta):
 		_animated_sprite_Idle.frame = 0
 		time_elapsed = 0
 		
-		_animated_sprite_Jump.visible = true
-		_animated_sprite_Jump.play("NewJump")
+#		_animated_sprite_Jump.visible = true
+#		_animated_sprite_Jump.play("NewJump")
+		if(velocity.x >= 0):
+			_animated_sprite_Body.play("TorsoRunAnim")
+			_animated_sprite_Body.flip_h = true
+			
+			_animated_sprite_RightArm.play("RightArmRun")
+			_animated_sprite_RightArm.flip_h = true
+			
+			_animated_sprite_LeftArm.play("LeftArmRun")
+			_animated_sprite_LeftArm.flip_h = true
+			
+			_animated_sprite_LeftLeg.play("LeftLegRun")
+			_animated_sprite_LeftLeg.flip_h = true
+			
+			_animated_sprite_RightLeg.play("RightLegRun")
+			_animated_sprite_RightLeg.flip_h = true
+		else:
+			_animated_sprite_Body.play("TorsoRunAnim")
+			_animated_sprite_Body.flip_h = true
+			
+			_animated_sprite_RightArm.play("RightArmRun")
+			_animated_sprite_RightArm.flip_h = true
+			
+			_animated_sprite_LeftArm.play("LeftArmRun")
+			_animated_sprite_LeftArm.flip_h = true
+			
+			_animated_sprite_LeftLeg.play("LeftLegRun")
+			_animated_sprite_LeftLeg.flip_h = true
+			
+			_animated_sprite_RightLeg.play("RightLegRun")
+			_animated_sprite_RightLeg.flip_h = true
+		if(velocity.y == 0):
+			isOnGround = 0
 		
 #		velocity.y = JUMP
 	else:
@@ -130,7 +166,7 @@ func _physics_process(delta):
 			_animated_sprite_RightLeg.visible = false
 			_animated_sprite_Jump.visible = false
 			
-			isOnGround = true
+			isOnGround = 0
 			
 			time_elapsed += delta
 			if(time_elapsed > 3):
@@ -139,9 +175,9 @@ func _physics_process(delta):
 	# Gravity
 	velocity.y = velocity.y + GRAVITY
 	
-	if (Input.is_action_just_pressed("Jump") && isOnGround):
+	if (Input.is_action_just_pressed("Jump") && isOnGround < 2):
 		velocity.y = JUMP
-		isOnGround = false
+		isOnGround = isOnGround + 1
 	
 	velocity = move_and_slide(velocity)
 	
