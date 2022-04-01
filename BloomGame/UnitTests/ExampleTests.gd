5b70e98240c12bf449d7f8a7ac85c1a46372ca57
Rdawg028 <verdouwr1827@my.uwstout.edu>
2022-04-01 10:49:57 -0500

    adding test stuff

---
+++ BloomGame/UnitTests/ExampleTests.gd
@@ -0,0 +1,17 @@
+extends WAT.Test
+
+
+#This sets name of test in the results view
+func title()->String:
+	return "Bloom Example Tests"
+	
+#ANY METHOD THAT STARTS WITH test_ is a test method
+
+func test_simple_example()->void:
+	
+	#can use describe to show this string instead of the method name in the results view
+	describe("Bloom Example Test")
+	
+	asserts.is_true(true)
+	
+
