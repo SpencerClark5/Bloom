2dd02154ad29e7a0d82c6288bf95efa6253d81a0
CamBrickl <79422034+CamBrickl@users.noreply.github.com>
2022-04-01 15:26:16 -0500

    New movement for each limb

--- BloomGame/Scripts/Movement.gd
+++ BloomGame/Scripts/Movement.gd
@@ -17,164 +17,294 @@ onready var _animated_sprite_Plant = $PlantingSprite
 var time_elapsed = 0.0
 
 var isOnGround = 0
+var isPlanting = 0
 	
 func _physics_process(delta):
 	
 	# Movement and animation 
 	if Input.is_action_pressed("Right"):
-		
+		#Temporary until idle anim is added
+		#Turns anims on
 		_animated_sprite_Idle.visible = false
 		_animated_sprite_Body.visible = true
 		_animated_sprite_LeftArm.visible = true
 		_animated_sprite_LeftLeg.visible = true
 		_animated_sprite_RightArm.visible = true
 		_animated_sprite_RightLeg.visible = true
-		_animated_sprite_Jump.visible = false
-		
+
 		_animated_sprite_Idle.stop()
 		_animated_sprite_Idle.frame = 0
 		time_elapsed = 0
+		
 		if(velocity.y == 0):
-			_animated_sprite_Body.play("TorsoRunAnim")
+			#Set each anim to run
+			
+			_animated_sprite_Body.animation = "TorsoRunAnim"
 			_animated_sprite_Body.flip_h = false
-			
-			_animated_sprite_RightArm.play("RightArmRun")
+
+			_animated_sprite_RightArm.animation = "RightArmRun"
 			_animated_sprite_RightArm.flip_h = false
-			
-			_animated_sprite_LeftArm.play("LeftArmRun")
+
+			_animated_sprite_LeftArm.animation = "LeftArmRun"
 			_animated_sprite_LeftArm.flip_h = false
-			
-			_animated_sprite_LeftLeg.play("LeftLegRun")
+
+			_animated_sprite_LeftLeg.animation = "LeftLegRun"
 			_animated_sprite_LeftLeg.flip_h = false
-			
-			_animated_sprite_RightLeg.play("RightLegRun")
+
+			_animated_sprite_RightLeg.animation = "RightLegAnim"
 			_animated_sprite_RightLeg.flip_h = false
 			
+			#play run anim
+			
+			_animated_sprite_Body.play("TorsoRunAnim")
+			_animated_sprite_RightArm.play("RightArmRun")
+			_animated_sprite_LeftArm.play("LeftArmRun")
+			_animated_sprite_LeftLeg.play("LeftLegRun")
+			_animated_sprite_RightLeg.play("RightLegAnim")
+			
 			isOnGround = 0
+			
 		else:
-			_animated_sprite_Idle.visible = false
-			_animated_sprite_Body.visible = false
-			_animated_sprite_LeftArm.visible = false
-			_animated_sprite_LeftLeg.visible = false
-			_animated_sprite_RightArm.visible = false
-			_animated_sprite_RightLeg.visible = false
-			_animated_sprite_Jump.visible = true
-			_animated_sprite_Jump.flip_h = false
-			_animated_sprite_Jump.play("NewJump")
+			_animated_sprite_Body.animation = "TorsoJumpAnim"
+			_animated_sprite_Body.flip_h = false
+
+			_animated_sprite_RightArm.animation = "RightArmJumpAnim"
+			_animated_sprite_RightArm.flip_h = false
+
+			_animated_sprite_LeftArm.animation = "LeftArmJumpAnim"
+			_animated_sprite_LeftArm.flip_h = false
+
+			_animated_sprite_LeftLeg.animation = "LeftLegJumpAnim"
+			_animated_sprite_LeftLeg.flip_h = false
+
+			_animated_sprite_RightLeg.animation = "RightLegJumpAnim"
+			_animated_sprite_RightLeg.flip_h = false
+			
+			
+			_animated_sprite_Body.play("TorsoJumpAnim")
+			_animated_sprite_RightArm.play("RightArmJumpAnim")
+			_animated_sprite_LeftArm.play("LeftArmJumpAnim")
+			_animated_sprite_LeftLeg.play("LeftLegJumpAnim")
+			_animated_sprite_RightLeg.play("RightLegJumpAnim")
 			
 		velocity.x += SPEED
 	
 	elif Input.is_action_pressed("Left"):
-		
+	#Temporary until idle anim is added
+		#Turns anims on
 		_animated_sprite_Idle.visible = false
 		_animated_sprite_Body.visible = true
 		_animated_sprite_LeftArm.visible = true
 		_animated_sprite_LeftLeg.visible = true
 		_animated_sprite_RightArm.visible = true
 		_animated_sprite_RightLeg.visible = true
-		_animated_sprite_Jump.visible = false
-		
+
 		_animated_sprite_Idle.stop()
 		_animated_sprite_Idle.frame = 0
 		time_elapsed = 0
+		
 		if(velocity.y == 0):
-			_animated_sprite_Body.play("TorsoRunAnim")
+			#Set each anim to run
+			
+			_animated_sprite_Body.animation = "TorsoRunAnim"
 			_animated_sprite_Body.flip_h = true
-			
-			_animated_sprite_RightArm.play("RightArmRun")
+
+			_animated_sprite_RightArm.animation = "RightArmRun"
 			_animated_sprite_RightArm.flip_h = true
-			
-			_animated_sprite_LeftArm.play("LeftArmRun")
+
+			_animated_sprite_LeftArm.animation = "LeftArmRun"
 			_animated_sprite_LeftArm.flip_h = true
-			
-			_animated_sprite_LeftLeg.play("LeftLegRun")
+
+			_animated_sprite_LeftLeg.animation = "LeftLegRun"
 			_animated_sprite_LeftLeg.flip_h = true
-			
-			_animated_sprite_RightLeg.play("RightLegRun")
+
+			_animated_sprite_RightLeg.animation = "RightLegAnim"
 			_animated_sprite_RightLeg.flip_h = true
 			
+			#play run anim
+			
+			_animated_sprite_Body.play("TorsoRunAnim")
+			_animated_sprite_RightArm.play("RightArmRun")
+			_animated_sprite_LeftArm.play("LeftArmRun")
+			_animated_sprite_LeftLeg.play("LeftLegRun")
+			_animated_sprite_RightLeg.play("RightLegAnim")
+			
 			isOnGround = 0
+			
 		else:
-			_animated_sprite_Idle.visible = false
-			_animated_sprite_Body.visible = false
-			_animated_sprite_LeftArm.visible = false
-			_animated_sprite_LeftLeg.visible = false
-			_animated_sprite_RightArm.visible = false
-			_animated_sprite_RightLeg.visible = false
-			_animated_sprite_Jump.visible = true
-			_animated_sprite_Jump.flip_h = true
-			_animated_sprite_Jump.play("NewJump")
-		
+			_animated_sprite_Body.animation = "TorsoJumpAnim"
+			_animated_sprite_Body.flip_h = true
+
+			_animated_sprite_RightArm.animation = "RightArmJumpAnim"
+			_animated_sprite_RightArm.flip_h = true
+
+			_animated_sprite_LeftArm.animation = "LeftArmJumpAnim"
+			_animated_sprite_LeftArm.flip_h = true
+
+			_animated_sprite_LeftLeg.animation = "LeftLegJumpAnim"
+			_animated_sprite_LeftLeg.flip_h = true
+
+			_animated_sprite_RightLeg.animation = "RightLegJumpAnim"
+			_animated_sprite_RightLeg.flip_h = true
+			
+			
+			_animated_sprite_Body.play("TorsoJumpAnim")
+			_animated_sprite_RightArm.play("RightArmJumpAnim")
+			_animated_sprite_LeftArm.play("LeftArmJumpAnim")
+			_animated_sprite_LeftLeg.play("LeftLegJumpAnim")
+			_animated_sprite_RightLeg.play("RightLegJumpAnim")
 		velocity.x -= SPEED
 		
 	# Jump
 	elif Input.is_action_just_pressed("Jump"):
 		_animated_sprite_Idle.visible = false
-		_animated_sprite_Body.visible = false
-		_animated_sprite_LeftArm.visible = false
-		_animated_sprite_LeftLeg.visible = false
-		_animated_sprite_RightArm.visible = false
-		_animated_sprite_RightLeg.visible = false
+		_animated_sprite_Body.visible = true
+		_animated_sprite_LeftArm.visible = true
+		_animated_sprite_LeftLeg.visible = true
+		_animated_sprite_RightArm.visible = true
+		_animated_sprite_RightLeg.visible = true
+
+		_animated_sprite_Idle.stop()
+		_animated_sprite_Idle.frame = 0
+		time_elapsed = 0
+
+		if(velocity.x >= 0):
+			_animated_sprite_Body.animation = "TorsoJumpAnim"
+			_animated_sprite_Body.flip_h = false
+
+			_animated_sprite_RightArm.animation = "RightArmJumpAnim"
+			_animated_sprite_RightArm.flip_h = false
+
+			_animated_sprite_LeftArm.animation = "LeftArmJumpAnim"
+			_animated_sprite_LeftArm.flip_h = false
+
+			_animated_sprite_LeftLeg.animation = "LeftLegJumpAnim"
+			_animated_sprite_LeftLeg.flip_h = false
+
+			_animated_sprite_RightLeg.animation = "RightLegJumpAnim"
+			_animated_sprite_RightLeg.flip_h = false
+			
+			
+			_animated_sprite_Body.play("TorsoJumpAnim")
+			_animated_sprite_RightArm.play("RightArmJumpAnim")
+			_animated_sprite_LeftArm.play("LeftArmJumpAnim")
+			_animated_sprite_LeftLeg.play("LeftLegJumpAnim")
+			_animated_sprite_RightLeg.play("RightLegJumpAnim")
+			
+		else:
+			_animated_sprite_Body.animation = "TorsoJumpAnim"
+			_animated_sprite_Body.flip_h = true
+
+			_animated_sprite_RightArm.animation = "RightArmJumpAnim"
+			_animated_sprite_RightArm.flip_h = true
+
+			_animated_sprite_LeftArm.animation = "LeftArmJumpAnim"
+			_animated_sprite_LeftArm.flip_h = true
+
+			_animated_sprite_LeftLeg.animation = "LeftLegJumpAnim"
+			_animated_sprite_LeftLeg.flip_h = true
+
+			_animated_sprite_RightLeg.animation = "RightLegJumpAnim"
+			_animated_sprite_RightLeg.flip_h = true
+			
+			
+			_animated_sprite_Body.play("TorsoJumpAnim")
+			_animated_sprite_RightArm.play("RightArmJumpAnim")
+			_animated_sprite_LeftArm.play("LeftArmJumpAnim")
+			_animated_sprite_LeftLeg.play("LeftLegJumpAnim")
+			_animated_sprite_RightLeg.play("RightLegJumpAnim")
+			
+
+		if(velocity.y == 0):
+			isOnGround = 0
+
+	elif Input.is_action_pressed("Plant"):
 		
+		isPlanting = 1
+		
+		_animated_sprite_Idle.visible = false
+		_animated_sprite_Body.visible = true
+		_animated_sprite_LeftArm.visible = true
+		_animated_sprite_LeftLeg.visible = true
+		_animated_sprite_RightArm.visible = true
+		_animated_sprite_RightLeg.visible = true
+
 		_animated_sprite_Idle.stop()
 		_animated_sprite_Idle.frame = 0
 		time_elapsed = 0
 		
-#		_animated_sprite_Jump.visible = true
-#		_animated_sprite_Jump.play("NewJump")
 		if(velocity.x >= 0):
-			_animated_sprite_Body.play("TorsoRunAnim")
-			_animated_sprite_Body.flip_h = true
+			_animated_sprite_Body.animation = "TorsoPlantAnim"
+			_animated_sprite_Body.flip_h = false
+
+			_animated_sprite_RightArm.animation = "RightArmPlantAnim"
+			_animated_sprite_RightArm.flip_h = false
+
+			_animated_sprite_LeftArm.animation = "LeftArmPlantAnim"
+			_animated_sprite_LeftArm.flip_h = false
+
+			_animated_sprite_LeftLeg.animation = "LeftLegPlantAnim"
+			_animated_sprite_LeftLeg.flip_h = false
+
+			_animated_sprite_RightLeg.animation = "RightLegPlantAnim"
+			_animated_sprite_RightLeg.flip_h = false
 			
-			_animated_sprite_RightArm.play("RightArmRun")
-			_animated_sprite_RightArm.flip_h = true
 			
-			_animated_sprite_LeftArm.play("LeftArmRun")
-			_animated_sprite_LeftArm.flip_h = true
+			_animated_sprite_Body.play("TorsoPlantAnim")
+			_animated_sprite_RightArm.play("RightArmPlantAnim")
+			_animated_sprite_LeftArm.play("LeftArmPlantAnim")
+			_animated_sprite_LeftLeg.play("LeftLegPlantAnim")
+			_animated_sprite_RightLeg.play("RightLegPlantAnim")
 			
-			_animated_sprite_LeftLeg.play("LeftLegRun")
-			_animated_sprite_LeftLeg.flip_h = true
-			
-			_animated_sprite_RightLeg.play("RightLegRun")
-			_animated_sprite_RightLeg.flip_h = true
 		else:
-			_animated_sprite_Body.play("TorsoRunAnim")
+			_animated_sprite_Body.animation = "TorsoPlantAnim"
 			_animated_sprite_Body.flip_h = true
-			
-			_animated_sprite_RightArm.play("RightArmRun")
+
+			_animated_sprite_RightArm.animation = "RightArmPlantAnim"
 			_animated_sprite_RightArm.flip_h = true
-			
-			_animated_sprite_LeftArm.play("LeftArmRun")
+
+			_animated_sprite_LeftArm.animation = "LeftArmPlantAnim"
 			_animated_sprite_LeftArm.flip_h = true
-			
-			_animated_sprite_LeftLeg.play("LeftLegRun")
+
+			_animated_sprite_LeftLeg.animation = "LeftLegPlantAnim"
 			_animated_sprite_LeftLeg.flip_h = true
-			
-			_animated_sprite_RightLeg.play("RightLegRun")
+
+			_animated_sprite_RightLeg.animation = "RightLegPlantAnim"
 			_animated_sprite_RightLeg.flip_h = true
-		if(velocity.y == 0):
+			
+			
+			_animated_sprite_Body.play("TorsoPlantAnim")
+			_animated_sprite_RightArm.play("RightArmPlantAnim")
+			_animated_sprite_LeftArm.play("LeftArmPlantAnim")
+			_animated_sprite_LeftLeg.play("LeftLegPlantAnim")
+			_animated_sprite_RightLeg.play("RightLegPlantAnim")
+			
 			isOnGround = 0
+			if(_animated_sprite_Body.frame == 4):
+				isPlanting = 0;
 		
-#		velocity.y = JUMP
+	#Idle anim
 	else:
-		if(velocity.y == 0):
-			_animated_sprite_Idle.visible = true
-			_animated_sprite_Body.visible = false
-			_animated_sprite_LeftArm.visible = false
-			_animated_sprite_LeftLeg.visible = false
-			_animated_sprite_RightArm.visible = false
-			_animated_sprite_RightLeg.visible = false
-			_animated_sprite_Jump.visible = false
-			
-			isOnGround = 0
-			
-			time_elapsed += delta
-			if(time_elapsed > 3):
-				_animated_sprite_Idle.play("Idle")
+#		if(isPlanting == 0):
+			if(velocity.y == 0):
+				_animated_sprite_Idle.visible = true
+				_animated_sprite_Body.visible = false
+				_animated_sprite_LeftArm.visible = false
+				_animated_sprite_LeftLeg.visible = false
+				_animated_sprite_RightArm.visible = false
+				_animated_sprite_RightLeg.visible = false
+				_animated_sprite_Jump.visible = false
+				
+				isOnGround = 0
+				
+				time_elapsed += delta
+				if(time_elapsed > 3):
+					_animated_sprite_Idle.play("Idle")
 		
 	# Gravity
 	velocity.y = velocity.y + GRAVITY
 	
+	#Player jump and double jump
 	if (Input.is_action_just_pressed("Jump") && isOnGround < 2):
 		velocity.y = JUMP
 		isOnGround = isOnGround + 1
