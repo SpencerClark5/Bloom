0ee94d2d912636dd72fe2781573370054db900c7
Rdawg028 <verdouwr1827@my.uwstout.edu>
2022-04-04 18:24:43 -0500

    trying to get keyframes to work

--- BloomGame/Scripts/Planting.gd
+++ BloomGame/Scripts/Planting.gd
@@ -5,19 +5,18 @@ extends Node
 # var a = 2
 # var b = "text"
 onready var _animated_sprite = $AnimatedSprite
+onready var _plant = get_node("StaticBody2D/Plant")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-	
 	_animated_sprite.play("PlantAnim")
+	_plant.disabled = true
+	#$StaticBody2D/Plant.disabled = true
 	##after the animation (yeild) turn on the collider
 	
 	
 	# maybe make another function to unplant
 	# just do everything in plant except backwards
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _enableCollider():
+	print ("Called!")
+	_plant.disabled = false
