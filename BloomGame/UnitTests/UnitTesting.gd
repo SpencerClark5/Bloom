cd8a90ceecc173a2dfb0b8a3a9a1443b35b24ce4
Rdawg028 <verdouwr1827@my.uwstout.edu>
2022-04-06 21:35:24 -0500

    more testing stuff

--- BloomGame/UnitTests/UnitTesting.gd
+++ BloomGame/UnitTests/UnitTesting.gd
@@ -1,13 +1,30 @@
 extends WAT.Test
-
+#Variables being used
 var _smallPlant = preload("res://Scenes/Plant.tscn")
+var _testFlora = preload("res://Scenes/Flora.tscn")
+var _game = preload("res://Scenes/Game.tscn")
 #This sets name of test in the results view
 func title()->String:
 	return "Bloom Example Tests"
 	
+#func start() ->void:
+	#var _smallPlant = preload("res://Scenes/Plant.tscn")
+	#var _testFlora = preload("res://Scenes/Flora.tscn")
+	
 #ANY METHOD THAT STARTS WITH test_ is a test method
 
+#tests that a plant is not null
+func test_for_Plant()->void:
+	_smallPlant.instance()
+	asserts.is_not_null(_smallPlant)
+
+#test that flora is not null
 func test_for_Flora()->void:
-	var _instanceNode = _smallPlant.instance()
+	_testFlora.instance()
+	asserts.is_not_null(_testFlora)
 	
-	asserts.is_not_null(_instanceNode)
+func test_for_Function_Call()->void:
+	_game.instance()
+	Input.action_press("Plant")
+	asserts.was_called(_game.Plant(), "was Called")
+	
