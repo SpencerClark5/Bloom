b18a18e9e0eebdb32ce7817decc33aa46c3b3b30
Rdawg028 <verdouwr1827@my.uwstout.edu>
2022-04-06 20:56:45 -0500

    adding unit tests

--- BloomGame/UnitTests/UnitTesting.gd
+++ BloomGame/UnitTests/UnitTesting.gd
@@ -1,6 +1,6 @@
 extends WAT.Test
 
-onready var node
+var _smallPlant = preload("res://Scenes/Plant.tscn")
 #This sets name of test in the results view
 func title()->String:
 	return "Bloom Example Tests"
@@ -8,8 +8,6 @@ func title()->String:
 #ANY METHOD THAT STARTS WITH test_ is a test method
 
 func test_for_Flora()->void:
+	var _instanceNode = _smallPlant.instance()
 	
-	#can use describe to show this string instead of the method name in the results view
-	describe("Bloom Example Test")
-	#node.get_node("Flora")
-	asserts.auto_pass()
+	asserts.is_not_null(_instanceNode)
