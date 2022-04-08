76660311ff021b999ba0c574aa56632086156ede
Rdawg028 <verdouwr1827@my.uwstout.edu>
2022-04-08 10:12:07 -0500

    adding plant sound

--- BloomGame/Scripts/Planting.gd
+++ BloomGame/Scripts/Planting.gd
@@ -7,12 +7,14 @@ extends Node
 #onready var _animated_sprite = $AnimatedSprite
 onready var _plant = get_node("StaticBody2D/Plant")
 onready var _animation_player = $AnimationPlayer
+onready var _music_player = $AudioStreamPlayer2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	#_animated_sprite.play("PlantAnim")
 	_animation_player.play("PlantAnim")
 	_plant.disabled = true
+	_music_player.play()
 	#$StaticBody2D/Plant.disabled = true
 	##after the animation (yeild) turn on the collider
 	
