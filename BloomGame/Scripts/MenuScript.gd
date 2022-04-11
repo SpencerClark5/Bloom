ed562942f124d1e99d77556842d9ad0d607caed6
Rdawg028 <verdouwr1827@my.uwstout.edu>
2022-04-11 17:31:44 -0500

    adding new font and making menus

---
+++ BloomGame/Scripts/MenuScript.gd
@@ -0,0 +1,24 @@
+extends Control
+
+
+onready var _controlsScene = "res://Scenes/Controls.tscn"
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Start_pressed():
+	get_tree().change_scene("res://Scenes/Game.tscn")
+	
+
+
+func _on_Controls_pressed():
+	get_tree().chagne_scene("res://Scenes/Controls.tscn")
