9c0ee6642dc0dd48859f8140e9ba5b1bbc4508a2
SpencerClark5 <spencerc5936@gmail.com>
2022-04-21 18:08:49 -0500

    big plant working

--- BloomGame/Scripts/bigplant.gd
+++ BloomGame/Scripts/bigplant.gd
@@ -1,16 +1,19 @@
-extends Area2D
+extends Node
 
 
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
+#onready var _animated_sprite = $AnimatedSprite
+onready var _plant = get_node("StaticBody2D/BigPlant")
+onready var _animation_player = $AnimationPlayer
+onready var _music_player = $AudioStreamPlayer2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	#_animated_sprite.play("PlantAnim")
+	_animation_player.play("Growing")
+	#_plant.disabled = true
+	#_music_player.play()
+	#$StaticBody2D/Plant.disabled = true
+	##after the animation (yeild) turn on the collider
