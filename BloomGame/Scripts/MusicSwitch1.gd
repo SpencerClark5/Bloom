a3268942ea7d0bd8135f2eacd62a2c7f74923dcd
Rdawg028 <verdouwr1827@my.uwstout.edu>
2022-04-29 16:57:03 -0500

    music

--- BloomGame/Scripts/MusicSwitch1.gd
+++ BloomGame/Scripts/MusicSwitch1.gd
@@ -18,15 +18,16 @@ func _ready():
 
 
 func _on_MusicSwitch1_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
-	print("in switch")
-	var level_2_music = AudioStreamPlayer.new()
-	level_2_music.stream = load("res://Music/hopeLOOP_Undergrowth_4-21-22_Bloom.wav")
-	_flora.add_child(level_2_music)
-	looping_music.stop()
-	looping_music.queue_free()
-	if not (level_2_music.is_playing()):
-		level_2_music.volume_db = -24
-		level_2_music.play()
+	if(GlobalVariables.canPlay):
+		print("in switch")
+		var level_2_music = AudioStreamPlayer.new()
+		level_2_music.stream = load("res://Music/hopeLOOP_Undergrowth_4-21-22_Bloom.wav")
+		_flora.add_child(level_2_music)
+		looping_music.stop()
+		if not (level_2_music.is_playing()):
+			level_2_music.volume_db = -24
+			level_2_music.play()
+		GlobalVariables.canPlay = false
 
 
 func _on_MusicSwitch1_body_shape_exited(body_rid, body, body_shape_index, local_shape_index):
