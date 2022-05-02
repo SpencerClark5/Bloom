9e16b8f468166dac8d3eae9fe2f64f17179265b9
CamBrickl <79422034+CamBrickl@users.noreply.github.com>
2022-05-01 23:35:02 -0500

    Can grow check

--- BloomGame/Scripts/Grow.gd
+++ BloomGame/Scripts/Grow.gd
@@ -5,59 +5,60 @@ onready var _collider_seed = $SeedCollider
 
 
 func _on_Area2D11_body_entered(body):
-	GlobalVariables.canGrow = true
-	print("in grow")
-	_sprite_seed.visible = false
-	_collider_seed.disabled = true
-	
-	GlobalVariables.isGrowing = true
-	
-	if(GlobalVariables.fourthLimb == "Leg"):
-		GlobalVariables.RightLegOn = true
-		GlobalVariables.canSingleJump = true
-		GlobalVariables.fourthLimb = "null"
-	elif(GlobalVariables.fourthLimb == "Arm"):
-		GlobalVariables.RightArmOn = true
-		GlobalVariables.canClimb = true
-		GlobalVariables.fourthLimb = "null"
+	if(GlobalVariables.timesPlanted != 0):
+		GlobalVariables.canGrow = true
+		print("in grow")
+		_sprite_seed.visible = false
+		_collider_seed.disabled = true
 		
-	elif(GlobalVariables.thirdLimb == "Leg"):
-		GlobalVariables.RightLegOn = true
-		GlobalVariables.canSingleJump = true
-		GlobalVariables.thirdLimb = "null"
-	elif(GlobalVariables.thirdLimb == "Arm"):
-		GlobalVariables.RightArmOn = true
-		GlobalVariables.canClimb = true
-		GlobalVariables.thirdLimb = "null"
+		GlobalVariables.isGrowing = true
 		
-	elif(GlobalVariables.secondLimb == "Leg"):
-		if(GlobalVariables.RightLegOn):
-			GlobalVariables.LeftLegOn = true
-			GlobalVariables.canDoubleJump = true
-		else:
+		if(GlobalVariables.fourthLimb == "Leg"):
 			GlobalVariables.RightLegOn = true
 			GlobalVariables.canSingleJump = true
-		GlobalVariables.secondLimb = "null"
-	elif(GlobalVariables.secondLimb == "Arm"):
-		if(GlobalVariables.RightArmOn):
-			GlobalVariables.LeftArmOn = true
-			GlobalVariables.canGrapple = true
-		else:
+			GlobalVariables.fourthLimb = "null"
+		elif(GlobalVariables.fourthLimb == "Arm"):
 			GlobalVariables.RightArmOn = true
 			GlobalVariables.canClimb = true
-		GlobalVariables.secondLimb = "null"
-		
-	elif(GlobalVariables.firstLimb == "Leg"):
-		GlobalVariables.LeftLegOn = true
-		GlobalVariables.canDoubleJump = true
-		GlobalVariables.firstLimb = "null"
-	elif(GlobalVariables.firstLimb == "Arm"):
-		GlobalVariables.LeftArmOn = true
-		GlobalVariables.canGrapple = true
-		GlobalVariables.firstLimb = "null"
-	queue_free()
-	if(GlobalVariables.timesPlanted != 0):
-		GlobalVariables.timesPlanted -= 1
+			GlobalVariables.fourthLimb = "null"
+			
+		elif(GlobalVariables.thirdLimb == "Leg"):
+			GlobalVariables.RightLegOn = true
+			GlobalVariables.canSingleJump = true
+			GlobalVariables.thirdLimb = "null"
+		elif(GlobalVariables.thirdLimb == "Arm"):
+			GlobalVariables.RightArmOn = true
+			GlobalVariables.canClimb = true
+			GlobalVariables.thirdLimb = "null"
+			
+		elif(GlobalVariables.secondLimb == "Leg"):
+			if(GlobalVariables.RightLegOn):
+				GlobalVariables.LeftLegOn = true
+				GlobalVariables.canDoubleJump = true
+			else:
+				GlobalVariables.RightLegOn = true
+				GlobalVariables.canSingleJump = true
+			GlobalVariables.secondLimb = "null"
+		elif(GlobalVariables.secondLimb == "Arm"):
+			if(GlobalVariables.RightArmOn):
+				GlobalVariables.LeftArmOn = true
+				GlobalVariables.canGrapple = true
+			else:
+				GlobalVariables.RightArmOn = true
+				GlobalVariables.canClimb = true
+			GlobalVariables.secondLimb = "null"
+			
+		elif(GlobalVariables.firstLimb == "Leg"):
+			GlobalVariables.LeftLegOn = true
+			GlobalVariables.canDoubleJump = true
+			GlobalVariables.firstLimb = "null"
+		elif(GlobalVariables.firstLimb == "Arm"):
+			GlobalVariables.LeftArmOn = true
+			GlobalVariables.canGrapple = true
+			GlobalVariables.firstLimb = "null"
+		queue_free()
+		if(GlobalVariables.timesPlanted != 0):
+			GlobalVariables.timesPlanted -= 1
 
 
 func _on_Area2D11_body_exited(body):
